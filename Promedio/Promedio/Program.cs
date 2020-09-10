36dea5c5fc911315e93031c84030899465743bdf
camilo20000403 <63866558+camilo20000403@users.noreply.github.com>
2020-09-09 21:00:19 -0500

    Add files via upload

---
+++ Promedio/Promedio/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Promedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float nota1, nota2, nota3, promedio;
+            String linea;
+                 Console.WriteLine("Digite la nota 1");
+            linea = Console.ReadLine();
+
+            nota1 = float.Parse(linea);
+            Console.WriteLine("Digite la nota 2");
+            linea = Console.ReadLine();
+
+            nota2 = float.Parse(linea);
+            Console.WriteLine("Digite la nota 3");
+            linea = Console.ReadLine();
+
+            nota3 = float.Parse(linea);
+
+            promedio = (nota1 + nota2 + nota3) / 3;
+            if (promedio < 3)
+            {
+                Console.WriteLine("REPROBO su Promedio es " + promedio);
+
+            }
+            else if (promedio >= 3)
+            {
+                Console.WriteLine("APROBO su Promedio es " + promedio);
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+    }
